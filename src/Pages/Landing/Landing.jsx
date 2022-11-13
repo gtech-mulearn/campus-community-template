afb15d0d66e79b0a59202e0f85884113c264abbd
Aswin <aswinasokofficail@gmail.com>
2022-11-13 21:12:54 +0530

    navbar: development

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -6,8 +6,21 @@ import styles from "./Landing.module.css";
 const Landing = () => {
   return (
     <>
-      <Navbar />
-      <EventsCard/>
+      <div className={styles.main_conatiner}>
+        <div className={styles.first_view_container}>
+          <Navbar />
+          <div className={styles.first_view_texts}>
+            <div className={styles.first_view_heading}>GTech MuLearn CUCEK</div>
+            <div className={styles.first_view_description}>
+              A nonprofit network of high school coding clubs and makers around
+              the world, for the students, by the students.
+            </div>
+            <div className={styles.join_button}>
+              <button className={styles.join_campus_button}>Join Us</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
